62c94668b1c17bdb28319fb5deee002f36028ec7
ayebaishmo <ishmo255super@gmail.com>
2025-04-20 11:34:18 +0300

    tests passed

--- greeting/config/urls.py
+++ greeting/config/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, wrong_view
 
 urlpatterns = [
     path("", index, name="index"),
+    path('wrong/', wrong_view, name='wrong'),
 ]
 
